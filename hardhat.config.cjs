0d0613217583d82b39e7e40498e8a5786925c6f4
Adrien Zaganelli <adrienzaganelli@gmail.com>
2025-06-30 19:15:56 +0200

    fix

---
+++ hardhat.config.cjs
@@ -0,0 +1,16 @@
+/**
+ * @file Hardhat configuration file
+ * @module hardhat.config
+ */
+
+require("@nomicfoundation/hardhat-toolbox");
+
+/**
+ * Hardhat configuration object.
+ * @type {import('hardhat/config').HardhatUserConfig}
+ */
+const config = {
+  solidity: "0.8.24",
+};
+
+module.exports = config;module.exports = config;
