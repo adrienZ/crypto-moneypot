a5dda3765464c55832e908040a9315652b31737b
Adrien Zaganelli <adrienzaganelli@gmail.com>
2025-07-05 00:28:58 +0200

    more options in rich text editor

--- app/components/RichTextEditor.vue
+++ app/components/RichTextEditor.vue
@@ -5,16 +5,47 @@
 
 <script setup lang="ts">
 import Quill from "quill";
-import { onMounted, useTemplateRef } from "vue";
+import { onMounted, onUnmounted, useTemplateRef, ref } from "vue";
 
 const root = useTemplateRef("root");
+const quillInstance = ref<Quill>();
 
 onMounted(() => {
-  new Quill(root.value!, {
+  const toolbarOptions = [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote"],
+    // ["blockquote", "code-block"],
+    ["link", "image", "video", "formula"],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ["clean"], // remove formatting button
+  ];
+
+  quillInstance.value = new Quill(root.value!, {
     theme: "snow",
     modules: {
-      toolbar: ["bold", "italic", "underline", "strike"],
+      toolbar: toolbarOptions,
     },
   });
+});
+
+onUnmounted(() => {
+  quillInstance.value?.disable();
+  if (quillInstance.value) {
+    quillInstance.value.root.innerHTML = "";
+  }
+  quillInstance.value = undefined;
 });
 </script>
